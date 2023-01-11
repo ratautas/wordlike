58b86990b4c43a03853af4eab34ed7d3718323c9
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-01-11 06:14:01 +0200

    Add login

--- apps/builder/src/routes/+page.svelte
+++ apps/builder/src/routes/+page.svelte
@@ -1,40 +1,31 @@
 <script lang="ts">
-  import doc from "$lib/mock/doc.json";
-  import Text from "$lib/components/Text.svelte";
+  import { enhance, type SubmitFunction } from "$app/forms";
+  import { supabaseClient } from "$lib/supabase";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  const submitLogout: SubmitFunction = async ({ cancel }) => {
+    const { error } = await supabaseClient.auth.signOut();
+    if (error) {
+      console.log(error);
+    }
+    cancel();
+  };
 </script>
 
-{#if data.session}
-  <p>Welcome, {data.session.user.email}</p>
-{:else}
-  <a href="/login">Log in</a>
-  <a href="/register">Register</a>
-{/if}
-
-{#each doc.children as component}
-  {#if component.type === "GRID"}
-    <div class="grid">
-      {#each component.children as item}
-        {#if item.type === "TEXT"}
-          <Text {item} />
-        {:else}
-          <h1>Welcome to SvelteKit</h1>
-          <p>
-            Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-            documentation
-          </p>
-        {/if}
-      {/each}
+<main>
+  <h1>SvelteKit & Supabase Auth</h1>
+  {#if data.session}
+    <p>Welcome, {data.session.user.email}</p>
+    <form action="/logout" method="POST" use:enhance={submitLogout}>
+      <button type="submit" class="btn btn-primary">Logout</button>
+    </form>
+  {:else}
+    <p>Let's learn how to register and login users!</p>
+    <div class="auth-buttons">
+      <a href="/login" class="btn btn-primary">Login</a>
+      <a href="/register" class="btn btn-secondary">Register</a>
     </div>
   {/if}
-{/each}
-
-<style>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-gap: 24px;
-  }
-</style>
+</main>
--- apps/builder/src/routes/login/+page.svelte
+++ apps/builder/src/routes/login/+page.svelte
@@ -1,13 +1,10 @@
-<script lang="ts">
-</script>
-
 <main>
-  <h1>Register</h1>
-  <form action="?/register" method="POST" class="auth-form">
+  <h1>Login</h1>
+  <form action="?/login" method="POST" class="auth-form">
     <label for=""> Email </label>
     <input type="text" name="email" />
     <label for=""> Password </label>
     <input type="password" name="password" />
-    <button class="btn btn-primary">Register</button>
+    <button type="submit" class="btn btn-primary">Login</button>
   </form>
 </main>
