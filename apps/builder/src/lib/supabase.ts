66f61ae4a122921949aaa39f3e95437dc48ed14d
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-01-11 10:26:34 +0200

    Options to constant

--- apps/builder/src/lib/supabase.ts
+++ apps/builder/src/lib/supabase.ts
@@ -1,14 +1,16 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
+const OPTIONS = {
+  realtime: {
+    params: {
+      eventsPerSecond: 1,
+    }
+  }
+};
+
 export const supabaseClient = createClient(
   PUBLIC_SUPABASE_URL,
   PUBLIC_SUPABASE_ANON_KEY,
-  {
-    realtime: {
-      params: {
-        eventsPerSecond: 1,
-      }
-    }
-  }
+  OPTIONS
 );
