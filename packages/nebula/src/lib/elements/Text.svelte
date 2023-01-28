2fa1c14a65a7c610975d32acb78d02f9e1beaeaf
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-01-28 15:59:46 +0200

    Add some styles

--- packages/nebula/src/lib/elements/Text.svelte
+++ packages/nebula/src/lib/elements/Text.svelte
@@ -9,10 +9,29 @@
 {/if}
 
 <style lang="scss">
-	:global(.text[contenteditable='true']) {
-		outline: none;
-		&:focus {
+	.text {
+		&[contenteditable='true'] {
 			outline: none;
+			&:focus {
+				outline: none;
+			}
+		}
+
+		:global(h1) {
+			font-size: 2rem;
+			font-weight: 700;
+		}
+		:global(h2) {
+			font-size: 1.5rem;
+			font-weight: 700;
+		}
+		:global(h3) {
+			font-size: 1.3rem;
+			font-weight: 700;
+		}
+		:global(h4) {
+			font-size: 1rem;
+			font-weight: 700;
 		}
 	}
 </style>
