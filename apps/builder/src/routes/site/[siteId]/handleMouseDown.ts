8d3ac245c429dd6bf80f84b3a9a987c73a19ecfa
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2023-02-12 13:27:41 +0200

    Cleanup handleMouseDown

--- apps/builder/src/routes/site/[siteId]/handleMouseDown.ts
+++ apps/builder/src/routes/site/[siteId]/handleMouseDown.ts
@@ -1,15 +1,2 @@
-import { get } from "svelte/store";
-
-import { refStore } from "$lib/actions/ref";
-
-import {
-    initialMousePosition,
-    isDragging,
-} from "$lib/stores/drag";
-import {
-    selectedElementIds,
-} from "$lib/stores/element";
-import { isShiftPressed } from "$lib/stores/keys";
-
 export function handleMouseDown(event: MouseEvent) {
 };
